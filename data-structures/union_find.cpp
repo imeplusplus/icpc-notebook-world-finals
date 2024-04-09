bba3a8ed48c2fe0335d98e1509e107e849ed5ba4
srmacario <sergioreinier@gmail.com>
2024-04-08 21:58:44 -0300

    unifying union find

--- data-structures/union_find.cpp
+++ data-structures/union_find.cpp
@@ -7,7 +7,8 @@
 *           sz:  vector of subsets sizes, i.e. size of the subset a node is in    *
 **********************************************************************************/
 
-int par[N], sz[N];
+int par[N], sz[N], his[N];
+stack <pii> sp, ss;
 
 int find(int a) { return par[a] == a ? a : par[a] = find(par[a]); }
 
@@ -17,6 +18,38 @@ void unite(int a, int b) {
 	par[b] = a; sz[a] += sz[b];
 }
 
-// in main
-for (int i = 1; i <= n; i++) par[i] = i, sz[i] = 1;
- 
+//in main
+for(int i = 0; i < N; i++) par[i] = i, sz[i] = 1, his[i] = 0;
+
+//Rollback
+int find (int a) { return par[a] == a ? a : find(par[a]); }
+
+void unite (int a, int b) {
+	if ((a = find(a)) == (b = find(b))) return;
+	if (sz[a] < sz[b]) swap(a, b);
+	ss.push({a, sz[a]});
+	sp.push({b, par[b]});
+	sz[a] += sz[b];
+	par[b] = a;
+}
+
+void rollback() {
+	par[sp.top().st] = sp.top().nd; sp.pop();
+	sz[ss.top().st]  = ss.top().nd; ss.pop();
+}
+
+//Partial Persistence
+int t, par[N], sz[N]
+
+int find(int a, int t){
+	if(par[a] == a) return a;
+	if(his[a] > t) return a;
+	return find(par[a], t);
+}
+
+void unite(int a, int b){
+	if(find(a, t) == find(b, t)) return;
+	a = find(a, t), b = find(b, t), t++;
+	if(sz[a] < sz[b]) swap(a, b);
+	sz[a] += sz[b], par[b] = a, his[b] = t;
+}
