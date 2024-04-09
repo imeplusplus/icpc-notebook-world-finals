85e4a9dcd0416473db7b41280d164f03f28449b9
srmacario <sergioreinier@gmail.com>
2024-04-08 21:59:36 -0300

    removing tarjan and swapping manacher 2 with manacher

--- strings/manacher.cpp
+++ strings/manacher.cpp
@@ -1,24 +1,33 @@
-// Manacher (Longest Palindromic String) - O(n)
-int lps[2*N+5];
-char s[N];
+// Manacher O(n)
 
-int manacher() {
-	int n = strlen(s);
+vector<int> d1, d2;
 
-	string p (2*n+3, '#');
-	p[0] = '^';
-	for (int i = 0; i < n; i++) p[2*(i+1)] = s[i];
-	p[2*n+2] = '$';
+// d1 -> odd : size = 2 * d1[i] - 1, palindrome from i - d1[i] + 1 to i + d1[i] - 1
+// d2 -> even : size = 2 * d2[i], palindrome from i - d2[i] to i + d2[i] - 1
 
-	int k = 0, r = 0, m = 0;
-	int l = p.length();
-	for (int i = 1; i < l; i++) {
-		int o = 2*k - i;
-		lps[i] = (r > i) ? min(r-i, lps[o]) : 0;
-		while (p[i + 1 + lps[i]] == p[i - 1 - lps[i]]) lps[i]++;
-		if (i + lps[i] > r) k = i, r = i + lps[i];
-		m = max(m, lps[i]);
+void manacher(string &s) {
+	int n = s.size();
+	d1.resize(n), d2.resize(n);
+	for(int i = 0, l1 = 0, l2 = 0, r1 = -1, r2 = -1; i < n; i++) {
+		if(i <= r1) {
+			d1[i] = min(d1[r1 + l1 - i], r1 - i + 1);
+		}
+		if(i <= r2) {
+			d2[i] = min(d2[r2 + l2 - i + 1], r2 - i + 1);
+		}
+		while(i - d1[i] >= 0 and i + d1[i] < n and s[i - d1[i]] == s[i + d1[i]]) {
+			d1[i]++;
+		}
+		while(i - d2[i] - 1 >= 0 and i + d2[i] < n and s[i - d2[i] - 1] == s[i + d2[i]]) {
+			d2[i]++;
+		}
+		if(i + d1[i] - 1 > r1) {
+			l1 = i - d1[i] + 1;
+			r1 = i + d1[i] - 1;
+		}
+		if(i + d2[i] - 1 > r2) {
+			l2 = i - d2[i];
+			r2 = i + d2[i] - 1;
+		}
 	}
-	return m;
 }
-
